8a53cdc8b17edb79a89477b9556dc7344a1b22c8
Jasmine Noack <jasminenoack@gmail.com>
2015-02-26 12:28:58 -0500

    makes the kitties slide

--- carousel/cats.js
+++ carousel/cats.js
@@ -13,22 +13,31 @@
     }.bind(this));
   };
 
-  $.Carousel.prototype.slide = function (dir) {
+  $.Carousel.prototype.slide = function (dir, classDir) {
     var carouselLength = this.$items.length
     this.$items.removeClass("active")
 
     // javascript is terrible so we have to do this ridiculous work around to make the modulo operator work appropriately.
     this.activeIdx = (this.activeIdx + dir + carouselLength) % carouselLength
+    var right = (this.activeIdx + 1 + carouselLength) % carouselLength
+    var left = (this.activeIdx -1 + carouselLength) % carouselLength
 
-    this.$items.eq(this.activeIdx).addClass("active")
+    this.$items.eq(this.activeIdx).addClass("active " + classDir);
+    // this.$items.eq(left).addClass("left");
+    // this.$items.eq(right).addClass("right");
+    setTimeout( function () {
+      // this.$items.eq(left).removeClass("left");
+      // this.$items.eq(right).removeClass("right");
+      this.$items.eq(this.activeIdx).removeClass(classDir);
+    }.bind(this))
   }
 
   $.Carousel.prototype.slideLeft = function () {
-    this.slide(-1);
+    this.slide(-1, "left");
   };
 
   $.Carousel.prototype.slideRight = function () {
-    this.slide(1);
+    this.slide(1, "right");
   };
 
 
