1697b9c76a98cf692f74a6100b4dbcd044cd1d6b
Jasmine Noack <jasminenoack@gmail.com>
2015-02-26 18:04:06 -0500

    complete thumbnails

--- thumbnails/thumbnails.js
+++ thumbnails/thumbnails.js
@@ -1,14 +1,18 @@
 ;(function () {
 
-$.Thumbnails = function ($el) {
-  this.$el = $el;
-  this.$activeImg = $($el.find(".active"));
+$.Thumbnails = function (el) {
+  this.$el = $(el);
+  this.$activeImg = this.$el.find(".bigimages .active");
   this.bindClick();
   this.bindMouse();
+  this.thumbnailIdx = 0;
+  this.$images   = this.$el.find(".thumbnails li");
+  this.thumbnailBindClick();
+  this.fillThumbnails();
 };
 
 $.Thumbnails.prototype.activate = function ($selectedImg, setActive) {
-  this.$el.find("li").removeClass("active");
+  this.$el.find(".bigimages li").removeClass("active");
   if (setActive) {
     this.$activeImg = $selectedImg;
   }
@@ -28,25 +32,67 @@ $.Thumbnails.prototype.bindClick = function () {
 
 $.Thumbnails.prototype.bindMouse = function () {
   var thumbnails = this;
+  console.log(this.$el.find(".thumbnails"))
 
-  this.$el.find(".thumbnails").on("mouseenter", "li", function (event) {
+  this.$el.find(".thumbnails").on("mouseover", "li", function (event) {
+    console.log("mouseover")
     var $selectedImg = thumbnails.findSelectedImg($(this))
 
     thumbnails.activate($selectedImg, false);
   });
 
-  this.$el.find(".thumbnails").on("mouseleave", "li", function (event) {
-    thumbnails.activate(thumbnails.$activeImg, false);
-  });
-
-}
+  this.$el.find(".thumbnails").on("mouseleave", "li",
+      this.resetActiveImg.bind(this));
+};
 
 $.Thumbnails.prototype.findSelectedImg = function ($li) {
   var activeIdx = $li.index();
   return this.$el.find(".bigimages li").eq(activeIdx);
+};
+
+$.Thumbnails.prototype.resetActiveImg = function () {
+  console.log("reset")
+  this.activate(this.$activeImg, false);
+};
+
+$.Thumbnails.prototype.fillThumbnails = function () {
+  var i;
+  this.$images.removeClass("active foo");
+
+  for (i = 0; i < 4; i++) {
+    this.$images.eq(i + this.thumbnailIdx).addClass("active");
+  }
+};
+
+$.Thumbnails.prototype.thumbnailBindClick = function () {
+  var $buttons = this.$el.find(".thumbnail-nav button");
+  var that = this;
+
+  $buttons.on("click", function (event) {
+    console.log($(this).index());
+    if ( $(this).index() === 0 ) {
+      that.moveThumbnails(-1);
+    } else {
+      that.moveThumbnails(1);
+    };
+  });
 }
 
+$.Thumbnails.prototype.moveThumbnails = function (dir) {
+  if ( (dir === -1 && this.thumbnailIdx === 0) ||
+    (dir === 1 && this.thumbnailIdx === this.$images.length - 4)) {
+      return;
+    };
 
+  this.$images.removeClass();
+  this.thumbnailIdx += dir;
 
+  this.fillThumbnails();
+};
 
+$.fn.Thumbnails = function () {
+  return this.each(function () {
+    new $.Thumbnails(this);
+  });
+};
 })();
