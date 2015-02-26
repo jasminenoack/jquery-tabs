89a4f9f9fe96b9ea53f0736dae0824a44f163a9c
Jasmine Noack <jasminenoack@gmail.com>
2015-02-26 10:40:48 -0500

    working tabs

--- corgitabs/tabs.js
+++ corgitabs/tabs.js
@@ -1,7 +1,25 @@
-$.Tabs = function (el) { ... };
+;(function () {
+
+$.Tabs = function (el) {
+  this.$el          = $(el);
+  this.$contentTabs = $(this.$el.data("content-tabs"));
+  this.$activeTab   = $(this.$contentTabs.find(".active"));
+
+  this.$el.on("click", "a", $.Tabs.clickTab.bind(this));
+};
 
 $.fn.tabs = function () {
   return this.each(function () {
     new $.Tabs(this);
   });
 };
+
+$.Tabs.clickTab = function (event) {
+  event.preventDefault();
+
+  this.$activeTab.removeClass("active");
+  this.$activeTab = $($(event.currentTarget).attr("href"));
+  this.$activeTab.addClass("active");
+};
+
+})();
